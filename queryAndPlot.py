0407e7d7ebc2048a47147718e8aef46280324f6d
YourDaddyIsHere <wadbl001@gmail.com>
2017-03-23 11:53:33 +0100

    Add files via upload

---
+++ queryAndPlot.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+from numpy.random import normal
+import os
+import sys
+if sys.platform == "darwin":
+    # Workaround for annoying MacOS Sierra bug: https://bugs.python.org/issue27126
+    # As fix, we are using pysqlite2 so we can supply our own version of sqlite3.
+    import pysqlite2.dbapi2 as sqlite3
+else:
+    import sqlite3
+
+conn = sqlite3.connect('load_balancing.db')
+c = conn.cursor()
+c.execute("select min1,min2 from visit")
+list_visit = c.fetchall()
+
+list_min1=[]
+for tuple in list_visit:
+	list_min1.append(tuple[0])
+
+plt.hist(list_min1)
+plt.title("Load Balancing Histogram")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.show()
---
+++ run.sh
@@ -0,0 +1,1 @@
+gumby/run.py gumby/load_balancing.conf
